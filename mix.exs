a95ff438437049c6f9076d92421955f5c4f376e6
Nicolas Brière <n.briere@smartway.ai>
2025-04-02 09:22:56 +0200

    fix(deps): include missing dependencies for ex_aws and jason

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule RabbitMQCloudWatchExporter.Mixfile do
       version: get_version(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps("deps"),
+      deps: deps(),
       deps_path: System.get_env("DEPS_DIR", "deps"),
       aliases: aliases()
     ]
@@ -28,13 +28,24 @@ defmodule RabbitMQCloudWatchExporter.Mixfile do
     |> String.replace_prefix("v", "") 
   end
 
-  defp deps(deps_dir) do
+  defp deps() do
     [
       {:ex_aws, "~> 2.5.7"},
       {:ex_aws_cloudwatch, "~> 2.0.4"},
       {:singleton, "~> 1.4.0"},
       {:jason, "~> 1.4.4"},
+      {:decimal, "~> 2.0"},
       {:hackney, "~> 1.20.1"},
+      {:sweet_xml, "~> 0.6"},
+      {:configparser_ex, "~> 4.0"},
+      {:req, "~> 0.5.10"},
+      {:castore, "~> 1.0"},
+      {:brotli, "~> 0.3"},
+      {:ezstd, "~> 1.0"},
+      {:nimble_csv, "~> 1.2"},
+      {:plug, "~> 1.0"},
+      {:mime, "~> 2.0.6"},
+      {:finch, "~> 0.17"},
       {:mix_task_archive_deps, github: "noxdafox/mix_task_archive_deps"}
     ]
   end
